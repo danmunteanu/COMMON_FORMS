30a9fe9b11183fb79d260b832e8d05a7edb3fd9f
Dan Munteanu (DELL) <epsdel@gmail.com>
2024-11-19 15:45:12 +0200

    Can add date prefix on TAB_Update

--- Utils.cs
+++ Utils.cs
@@ -34,6 +34,30 @@ namespace CommonForms
 
         //  load condition names' list
 
+        /*
+         *  Does a string start with a date prefix?
+         *      e.g. 2024-02-01
+         */
+        public static bool HasDatePrefix(string s)
+        {
+            //  Try cast first 10 characters to a date 2024-02-01
+            if (s.Length > 11)
+            {
+                //  first 10 characters should be castable to a date
+                string titleDate = s.Substring(0, 10);
 
+                //  try to cast to a date
+                try
+                {
+                    DateTime dateCast = DateTime.Parse(titleDate);
+                }
+                catch (FormatException formatExcept)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
