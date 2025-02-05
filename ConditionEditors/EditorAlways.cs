9c381e08f2cfb9d43c137da1202851d9797a784e
Dan Munteanu <epsdel@gmail.com>
2025-02-05 19:32:54 +0200

    Always & forever.

---
+++ ConditionEditors/EditorAlways.cs
@@ -0,0 +1,19 @@
+﻿namespace CommonForms.ConditionEditors
+{
+    public class EditorAlways : EditorBase<string>
+    {
+        public EditorAlways()
+        {
+            // Create a Windows Forms label
+            var label = new System.Windows.Forms.Label
+            {
+                Text = "ALWAYS",
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter, // Center the text
+                Dock = System.Windows.Forms.DockStyle.Fill // Fill the UserControl with the label
+            };
+
+            // Add the label to the UserControl's controls collection
+            this.Controls.Add(label);
+        }
+    }
+}
