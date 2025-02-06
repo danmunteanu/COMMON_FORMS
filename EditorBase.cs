78e18e433fc1a7b36910dfdea0a60e02a0194b4c
Dan Munteanu <epsdel@gmail.com>
2025-02-06 15:08:51 +0200

    Simplified methods

--- EditorBase.cs
+++ EditorBase.cs
@@ -5,7 +5,7 @@ namespace CommonForms
     /*
      * The base class for all Condition and Action editors
      */
-    public class EditorBase<T> : UserControl
+    public abstract class EditorBase<T> : UserControl
     {
         //  override this in derived classes to validate the state of the  UI
         public virtual bool ValidateState()
@@ -14,11 +14,10 @@ namespace CommonForms
         }
 
         public virtual void LoadState(Condition<T> cond)
-        {
-            //  load the editor state from a condition
-            throw new NotImplementedException();
-        }
-
+            //  loads the editor state from a concrete condition
+            //  must override in derived classes
+            => throw new NotImplementedException();
+        
         public virtual void SaveState(Condition<T> cond)
         {
             //  Saves the editor's state into the condition
@@ -41,23 +40,15 @@ namespace CommonForms
         protected Stack<string> mErrorStack = new();
 
         public void PushError(string error)
-        {
-            mErrorStack.Push(error);
-        }
+            => mErrorStack.Push(error);
 
         public string PopError()
-        {
-            return mErrorStack.Pop();
-        }
+            => mErrorStack.Pop();
 
         public bool HasErrors()
-        {
-            return mErrorStack.Count > 0;
-        }
+            => mErrorStack.Count > 0;
 
         public void ClearErrors()
-        {
-            mErrorStack.Clear();
-        }
+            => mErrorStack.Clear();
     }
 }
