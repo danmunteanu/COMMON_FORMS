2218e6bf0db2ad63e998a1afcfc6be65d457b616
Dan Munteanu <epsdel@gmail.com>
2025-04-17 23:13:51 +0300

    Redone selection and improved transforms

---
+++ Components/TransformsListComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonForms.Components
+{
+    public partial class TransformsListComponent: ApplicationPageBase
+    {
+        public TransformsListComponent()
+        {
+            InitializeComponent();
+        }
+    }
+}
